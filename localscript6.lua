05f2a67ae4d7db87e186ab62205f44287282e90b
floreseternasoporte-creator <floreseternasoporte@gmail.com>
2026-02-04 19:22:52 -0600

    Add LocalScript for looping background music
    This LocalScript plays background music in an infinite loop.

---
+++ localscript6.lua
@@ -0,0 +1,12 @@
+-- LocalScript - Música en bucle infinito
+
+local SoundService = game:GetService("SoundService")
+
+local musica = Instance.new("Sound")
+musica.Name = "MusicaFondo"
+musica.SoundId = "rbxassetid://75287953952726"
+musica.Looped = true
+musica.Volume = 1 -- Puedes ajustar el volumen (0 a 10)
+musica.Parent = SoundService
+
+musica:Play()
